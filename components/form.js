620d611a87a289b28f0bf7477088c2bd04d6d432
Lololoshka2007 <72262398+Lololoshka2007@users.noreply.github.com>
2023-03-12 12:29:41 +0200

    Updates

--- components/form.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Form = props => (
-    <form onSubmit={props.weatherMethod}>
-        <input type='text' name='city' placeholder='City'/>
-        <button>Get the weather!</button>
-    </form>
-)
-export default Form
--- components/info.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Info=()=>(
-    <div>
-        <h2>Weather app</h2>
-        <p>Check the weather in our city!</p>
-    </div>
-)
-export default Info
